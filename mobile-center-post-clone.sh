924884990f35da9db2d9f13aee82e09f4a249423
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-01-26 20:42:34 +0300

    Update mobile-center-post-clone.sh

--- mobile-center-post-clone.sh
+++ mobile-center-post-clone.sh
@@ -2,3 +2,8 @@
 echo "============================================="
 echo "======= MOBILECENTER POSTCLONE SCRIPT ======="
 echo "============================================="
+
+echo "$secret_token"
+if [ $secret_token = "test" ]; then
+echo "variable is valid"
+fi
